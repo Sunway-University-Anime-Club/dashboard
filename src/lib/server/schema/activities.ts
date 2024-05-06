525cf7ae1885af8f098b3d9031df3ca83c12468e
Spimy <officialspyme@gmail.com>
2024-05-06 22:44:57 +0800

    👌 IMPROVE: Update activities schema

--- src/lib/server/schema/activities.ts
+++ src/lib/server/schema/activities.ts
@@ -4,8 +4,8 @@ import { memberInterestedActivities } from './members_activities';
 
 export const activities = pgTable('activities', {
 	id: serial('id').primaryKey(),
-	name: varchar('name', { length: 16 }),
-	examples: jsonb('examples').$type<string[]>()
+	name: varchar('name', { length: 64 }),
+	examples: jsonb('examples').$type<string[]>().default([]).notNull()
 });
 
 export const activitiesRelations = relations(activities, ({ many }) => ({
